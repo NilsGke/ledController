f49a7cbe16510915ed9de7856fb73f691afa9892
NilsGke <n.j.goeke@gmail.com>
2022-07-25 14:09:02 +0200

    added config

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ import { setup } from "./src/controller";
 const app = express();
 const port = process.env.PORT || 3030;
 
+export const CONFIG = {
+    ledRefreshRate: 200,
+};
 
 // api route
 app.use("/api", api);
@@ -13,12 +16,11 @@ app.use("/api", api);
 app.use(express.static("app/build"));
 // server react app
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/app/build/index.html');
+    res.sendFile(__dirname + "/app/build/index.html");
 });
 
 app.listen(port, () => {
     console.log(`\x1b[42mServer is running on port: ${port}\x1b[0m`);
 });
-
 
 setup();
