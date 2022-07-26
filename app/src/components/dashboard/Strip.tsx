428c33c880495904cacbc0d23fcd629bd837432d
NilsGke <n.j.goeke@gmail.com>
2022-07-26 16:11:26 +0200

    added different color pickers

--- app/src/components/dashboard/Strip.tsx
+++ app/src/components/dashboard/Strip.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { HuePicker, ColorResult, AlphaPicker } from "react-color";
+import { ColorResult, AlphaPicker } from "react-color";
 import { rgbStripType } from "../../../../src/ledStrip/types";
 import {
     InputLabel,
@@ -12,81 +12,95 @@ import {
 
 import "../../styles/dashboard/dashboardStrip.sass";
 import { effect } from "../../../../src/effects/effect";
+import HuePicker from "../HuePicker";
+import RgbPicker from "../RgbPicker";
+import BrightnessPicker from "../BrightnessPicker";
 
 type props = {
     data: rgbStripType;
     effects: effect[];
     refresh: () => void;
+    sliderType: sliderTypes;
 };
 
-const DashboardStrip: React.FC<props> = ({ data: strip, effects, refresh }) => {
-    const [color, setColor] = useState<ColorResult["rgb"]>({
-        r: strip.color.red,
-        g: strip.color.green,
-        b: strip.color.blue,
+export type sliderTypes = "hue" | "rgb" | "fancyRgb";
+
+const DashboardStrip: React.FC<props> = ({
+    data: strip,
+    effects,
+    refresh,
+    sliderType: fancySliders,
+}) => {
+    const [color, setColor] = useState<rgbStripType["color"]>({
+        red: strip.color.red,
+        green: strip.color.green,
+        blue: strip.color.blue,
     });
 
-    const [newColor, setNewColor] = useState<null | ColorResult["rgb"]>(null);
+    const [currentColor, setCurrentColor] = useState<
+        null | rgbStripType["color"]
+    >(null);
+    const [newColor, setNewColor] = useState<null | rgbStripType["color"]>(
+        null
+    );
+
+    const alpha = 1;
 
     const [effect, setEffect] = useState<effect | null>(null);
 
     useEffect(() => {
         setColor({
-            r: strip.color.red,
-            g: strip.color.green,
-            b: strip.color.blue,
+            red: strip.color.red,
+            green: strip.color.green,
+            blue: strip.color.blue,
         });
-        const newEffect = effects.find(eff => strip.effectName === eff.name);
-        setEffect(newEffect === undefined ? null : newEffect)
+        const newEffect = effects.find((eff) => strip.effectName === eff.name);
+        setEffect(newEffect === undefined ? null : newEffect);
     }, [strip]);
 
     useEffect(() => {
         if (newColor === null) return;
-        if (newColor !== color) {
-            const alpha = newColor.a === undefined ? 1 : newColor.a;
-            const c = {
-                r: Math.floor(newColor.r * alpha),
-                g: Math.floor(newColor.g * alpha),
-                b: Math.floor(newColor.b * alpha),
-            };
-            fetch(`/api/strips/${strip.name}/set/${c.r}/${c.g}/${c.b}`).then(
-                () => {
-                    refresh();
-                    setNewColor(null);
-                }
-            );
+        if (newColor !== currentColor) {
+            fetch(
+                `/api/strips/${strip.name}/set/color/${newColor.red}/${newColor.green}/${newColor.blue}`
+            ).then(() => {
+                refresh();
+                setCurrentColor(color);
+                setNewColor(null);
+            });
         }
     }, [newColor]);
 
     const selectRef = useRef<HTMLInputElement | null>(null);
     const stripRef = useRef<HTMLInputElement | null>(null);
 
-    color.a = color.a === undefined ? 1 : color.a;
-
-    const colorString = `rgb(${Math.floor(color.r * color.a)}, ${Math.floor(
-        color.g * color.a
-    )}, ${Math.floor(color.b * color.a)})`;
+    const colorString = `rgb(${Math.floor(color.red * alpha)}, ${Math.floor(
+        color.green * alpha
+    )}, ${Math.floor(color.blue * alpha)})`;
     const actualColorString = `rgb(${strip.color.red}, ${strip.color.green}, ${strip.color.blue})`;
 
     const changeEffect = (effectName: effect["name"]) => {
         const effect = effects.find((e) => e.name === effectName);
-        if (effect === undefined) return;
+        if (effect === undefined) return setEffect(null);
         setEffect(effect);
     };
-
-
 
     useEffect(() => {
         if (stripRef.current === null) return;
         if (effect === null) {
-            stripRef.current.getAnimations().forEach(animation => animation.cancel())
+            stripRef.current
+                .getAnimations()
+                .forEach((animation) => animation.cancel());
             return;
-        };
+        }
 
-        const animationKeyframes = effect.keyframes.map(frame => ({ boxShadow: `0px 0px 40px 0px rgb(${frame.color.red}, ${frame.color.green}, ${frame.color.blue})`, offset: (frame.step / 100) }))
+        const animationKeyframes = effect.keyframes.map((frame) => ({
+            boxShadow: `0px 0px 40px 0px rgb(${frame.color.red}, ${frame.color.green}, ${frame.color.blue})`,
+            offset: frame.step / 100,
+        }));
         const animationTiming = {
             duration: effect.duration,
-            iterations: 99999,
+            iterations: Infinity,
         };
         stripRef.current.animate(animationKeyframes, animationTiming);
     }, [effect]);
@@ -113,55 +127,69 @@ const DashboardStrip: React.FC<props> = ({ data: strip, effects, refresh }) => {
                     <h2>{strip.name}</h2>
                 </div>
                 <div className="colorInput">
-                    <HuePicker
-                        color={color}
-                        onChange={({ rgb }) => {
-                            setColor({ ...rgb, a: color.a });
-                            setEffect(null);
-                        }}
-                        onChangeComplete={({ rgb }) =>
-                            setNewColor({ ...rgb, a: color.a })
-                        }
-                    />
-                    <AlphaPicker
-                        color={color}
-                        onChange={({ rgb }) => {
-                            setColor({ ...color, a: rgb.a })
-                        }}
-                        onChangeComplete={({ rgb }) =>
-                            setNewColor({ ...color, a: rgb.a })
-                        }
-                    />
+                    {fancySliders === "hue" ? (
+                        <>
+                            <HuePicker
+                                color={color as rgbStripType["color"]}
+                                onChange={(color) => {
+                                    setColor(color);
+                                    setEffect(null);
+                                }}
+                                onChangeComplete={(color) => {
+                                    setNewColor(color);
+                                }}
+                            />
+                            <BrightnessPicker
+                                color={color as rgbStripType["color"]}
+                                onChange={(color) => {
+                                    setColor(color);
+                                    setEffect(null);
+                                }}
+                                onChangeComplete={(color) => {
+                                    setNewColor(color);
+                                }}
+                            />
+                        </>
+                    ) : (
+                        <RgbPicker
+                            color={color as rgbStripType["color"]}
+                            onChange={(color) => {
+                                setColor(color);
+                                setEffect(null);
+                            }}
+                            onChangeComplete={(color) => {
+                                setNewColor(color);
+                            }}
+                            fancySliders={fancySliders}
+                        />
+                    )}
                 </div>
                 <div className="effectContainer">
-                    <ThemeProvider
-                        theme={createTheme({
-                            palette: {
-                                mode: "dark",
-                            },
-                        })}
-                    >
-                        <FormControl sx={{ m: 1, minWidth: 120 }}>
-                            <InputLabel id="demo-simple-select-label">
-                                Effect
-                            </InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={effect === null ? "" : effect.name}
-                                label="Effect"
-                                onChange={(e) => changeEffect(e.target.value as string)}
-                                ref={selectRef}
-                            >
-                                <MenuItem value={""}>-</MenuItem>
-                                {effects.map((effect) => (
-                                    <MenuItem key={effect.name + effect.id} value={effect.name}>
-                                        {effect.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </ThemeProvider>
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">
+                            Effect
+                        </InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={effect === null ? "" : effect.name}
+                            label="Effect"
+                            onChange={(e) =>
+                                changeEffect(e.target.value as string)
+                            }
+                            ref={selectRef}
+                        >
+                            <MenuItem value={""}>-</MenuItem>
+                            {effects.map((effect) => (
+                                <MenuItem
+                                    key={effect.name + effect.id}
+                                    value={effect.name}
+                                >
+                                    {effect.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                 </div>
             </div>
         </div>
