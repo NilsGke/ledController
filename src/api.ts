87c5ba3203fefdacc0efe8c4e7c1c60dd9b4ad6f
NilsGke <n.j.goeke@gmail.com>
2022-07-26 16:08:44 +0200

    added check color and added alpha route

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { applyPreset, setup, strips } from "./controller";
 import { effects } from "./effects/effect";
-import { ledColorValue, rgbStripType } from "./ledStrip/types";
+import { ledValue, rgbStripType } from "./ledStrip/types";
 import { presets } from "./presets";
+import checkColor from "./util/checkColor";
 
 const router = express.Router();
 
@@ -16,42 +17,58 @@ router.get("/info/presets", (req, res) =>
     res.send(JSON.stringify({ presets }))
 );
 
-router.get("/strips/:name/set/:r/:g/:b", (req, res) => {
+// set strips
+router.get("/strips/:name/set/color/:r/:g/:b", (req, res) => {
     // get strip
     const strip = strips.find((strip) => strip.name === req.params.name);
-    if (strip === undefined) res.status(404).send("strip not found");
+    if (strip === undefined) return res.status(404).send("strip not found");
 
-    const colorValues = {
-        red: parseInt(req.params.r),
-        green: parseInt(req.params.g),
-        blue: parseInt(req.params.b),
+    const colorValues: rgbStripType["color"] = {
+        red: parseInt(req.params.r) as ledValue,
+        green: parseInt(req.params.g) as ledValue,
+        blue: parseInt(req.params.b) as ledValue,
     };
 
-    const colorArray = [colorValues.red, colorValues.green, colorValues.blue];
+    if (!checkColor(colorValues))
+        res.sendStatus(400).send(
+            "requested color values are invalid and do not match the required type (0-255)"
+        );
+
+    // set color
+    strip.setColors(colorValues).then(() => res.sendStatus(200));
+});
+
+// IDEA: remove this?
+router.get("/strips/:name/set/alpha/:a", (req, res) => {
+    console.log("setting alpha");
+
+    // get strip
+    const strip = strips.find((strip) => strip.name === req.params.name);
+    if (strip === undefined) return res.status(404).send("strip not found");
+
+    const alpha = parseInt(req.params.a);
 
     // check that color is between 0 and 255
-    colorArray.forEach((c, i) => {
-        if (c > 255 || c < 0)
-            return res
-                .status(400)
-                .send(
-                    `color: ${["red", "green", "blue"].at(
-                        i
-                    )}: ${c} is an invalid color`
-                );
-    });
+    if (alpha > 255 || alpha < 0)
+        return res
+            .status(400)
+            .send(`value: ${alpha} is an invalid value for property alpha`);
 
     // set color
     strip
-        ?.setColors(colorValues as rgbStripType["color"])
+        .setAlpha(alpha as rgbStripType["alpha"])
         .then(() => res.sendStatus(200));
 });
 
+// preset
 router.get("/presets/apply/:name", (req, res) => {
     const preset = presets.find((preset) => preset.name === req.params.name);
     if (preset === undefined) res.status(404).send("preset not found");
 
     applyPreset(undefined, req.params.name).then(() => res.sendStatus(200));
 });
+
+// TODO: all strips
+router.get("/strips/all/set/color/:r/:g/:b", (req, res) => {});
 
 export default router;
