139d64532bee2de148dc493a3334d13f75de8767
NilsGke <n.j.goeke@gmail.com>
2022-07-27 14:57:37 +0200

    converted to express server to serve static files on http and ws on wss

--- index.ts
+++ index.ts
@@ -1,51 +1,37 @@
-import { setup, strips } from "./src/controller";
+import { getInfoObject, setup, strips } from "./src/controller";
 import { port } from "./app/src/connectionConfig.json"
-import http from "http";
 import WebSocket from "ws";
 import messageHandler from "./src/messageHandler";
 import { presets } from "./src/presets";
 import { effects } from "./src/effects";
-import { fileResponse } from "./src/responseFile";
+import express from "express"
+
+const app = express();
 
 export const CONFIG = {
     ledRefreshRate: 200,
 };
 
+const server = app.listen(port)
 
-
-
-const server = http.createServer((request, response) => {
-    console.log((new Date()) + ' Received request for ' + request.url);
-
-
-    fileResponse(request.url || "", response)
-
-
-    response.end();
-
-})
-
-server.listen(port, () => {
-    console.log((new Date()) + " Server is listening on port 8080");
-})
+app.use(express.static(__dirname + '/app/build'));
 
 
 const wsServer = new WebSocket.Server({
-    server: server,
+    server
 })
 
 wsServer.on("connection", (connection) => {
-    console.log((new Date()) + " connection");
-
+    console.log("\x1b[32mconnection\x1b[0m");
 
     connection.on("message", (message) => messageHandler(message, connection))
-    connection.on("close", () => console.log((new Date()) + "closed\n"))
+    connection.on("close", () => console.log("\x1b[31mclosed\x1b[0m"))
 })
 
 
 export const sendDataUpdate = () => {
     wsServer.clients.forEach(client => {
-        client.send(JSON.stringify({ strips, presets, effects }))
+        client.send(JSON.stringify(getInfoObject()))
     })
 }
 
