70bdff4a739702403b6a78975e4376835652dd04
NilsGke <n.j.goeke@gmail.com>
2022-07-28 23:42:01 +0200

    formatting

--- src/ledStrip/index.ts
+++ src/ledStrip/index.ts
@@ -10,11 +10,13 @@ export default class rgbStrip {
     public name: stripType["name"];
     public id: stripType["id"];
     public ports: stripType["ports"];
-    public gpioPorts: { red: gpio.Gpio; green: gpio.Gpio; blue: gpio.Gpio } | undefined;
+    public gpioPorts:
+        | { red: gpio.Gpio; green: gpio.Gpio; blue: gpio.Gpio }
+        | undefined;
     public color: stripType["color"] = { red: 0, green: 0, blue: 0 };
     public alpha: stripType["alpha"] = 255;
     public effect: effect | null = null;
-    private effectRunning: boolean = false;
+    public effectRunning: boolean = false;
     private onOff: onOff;
 
     constructor(
@@ -35,30 +37,33 @@ export default class rgbStrip {
             };
         } catch (error) {
             console.log(error);
-            console.log("\x1b[31m error while initializing gpio ports. You are probably using a device where there are no gpio ports. Be careful, not everything is gonna work!\x1b[0m")
+            console.log(
+                "\x1b[31m error while initializing gpio ports. You are probably using a device where there are no gpio ports. Be careful, not everything is gonna work!\x1b[0m"
+            );
         }
     }
 
-    public setOnOff = (onOff: onOff): void => { this.onOff === onOff }
+    public setOnOff = (onOff: onOff): void => {
+        this.onOff === onOff;
+    };
 
-    public setColors(color: stripType["color"]): Promise<void> {
-        return new Promise((res, rej) => {
-            if (!checkColor(color)) {
-                console.warn("tried setting color that falls outside of the specified range!", color)
-                rej()
-            }
-            this.color = color;
-            sendDataUpdate();
-            this.effectRunning = false;
-            res();
-        });
+    public setColors(color: stripType["color"], notify: boolean = true): void {
+        if (!checkColor(color))
+            return console.warn(
+                "tried setting color that falls outside of the specified range!",
+                color
+            );
+
+        this.color = color;
+        if (notify) sendDataUpdate();
+        this.updateColors();
     }
 
     public setEffect(effect: effect): void {
         this.effect = effect;
 
         sendDataUpdate();
-        this.runEffect()
+        this.runEffect();
     }
 
     public runEffect(): void {
@@ -79,30 +84,36 @@ export default class rgbStrip {
         const timePassed = Date.now() - time;
 
         let timeInEffect = timePassed;
-        while (timeInEffect >= duration) timeInEffect -= duration
+        while (timeInEffect >= duration) timeInEffect -= duration;
 
         let prev: keyframe | undefined, next: keyframe | undefined;
 
         let timeInTransition = timeInEffect;
 
         for (let i = 0; i < keyframes.length - 1; i++) {
-            if (duration / 100 * keyframes[i].step <= timeInEffect && timeInEffect <= duration / 100 * keyframes[i + 1].step) {
+            if (
+                (duration / 100) * keyframes[i].step <= timeInEffect &&
+                timeInEffect <= (duration / 100) * keyframes[i + 1].step
+            ) {
                 // current transition
                 prev = keyframes[i];
                 next = keyframes[i + 1];
-                break
+                break;
             } else {
                 // previous transition
-                timeInTransition -= duration / 100 * keyframes[i + 1].step - duration / 100 * keyframes[i].step
+                timeInTransition -=
+                    (duration / 100) * keyframes[i + 1].step -
+                    (duration / 100) * keyframes[i].step;
             }
         }
 
-
         if (prev !== undefined && next !== undefined) {
-            const transTime = (duration / 100 * next.step) - (duration / 100 * prev.step)
+            const transTime =
+                (duration / 100) * next.step - (duration / 100) * prev.step;
 
-
-            const percentTransPassed = Math.round(100 / transTime * timeInTransition)
+            const percentTransPassed = Math.round(
+                (100 / transTime) * timeInTransition
+            );
             const colorDiff = {
                 red: Math.round(prev.color.red - next.color.red),
                 green: Math.round(prev.color.green - next.color.green),
@@ -110,49 +121,54 @@ export default class rgbStrip {
             };
 
             const addColor = {
-                red: Math.round(colorDiff.red * percentTransPassed / 100),
-                green: Math.round(colorDiff.green * percentTransPassed / 100),
-                blue: Math.round(colorDiff.blue * percentTransPassed / 100),
-            }
+                red: Math.round((colorDiff.red * percentTransPassed) / 100),
+                green: Math.round((colorDiff.green * percentTransPassed) / 100),
+                blue: Math.round((colorDiff.blue * percentTransPassed) / 100),
+            };
 
             const newColor = {
                 red: prev.color.red - addColor.red,
                 green: prev.color.green - addColor.green,
                 blue: prev.color.blue - addColor.blue,
-            }
+            };
 
             // just to make sure the colors are not above 255
             newColor.red = newColor.red > 255 ? 255 : newColor.red;
             newColor.green = newColor.green > 255 ? 255 : newColor.green;
             newColor.blue = newColor.blue > 255 ? 255 : newColor.blue;
 
-            console.log(`%c ${newColor.red} ${newColor.green} ${newColor.blue}`, `background: rgb(${newColor.red}, ${newColor.green}, ${newColor.blue})`);
+            console.log(
+                `%c ${newColor.red} ${newColor.green} ${newColor.blue}`,
+                `background: rgb(${newColor.red}, ${newColor.green}, ${newColor.blue})`
+            );
 
             // IDEA: add cubic function to make transitions more interesting
-            // https://blog.maximeheckel.com/posts/cubic-bezier-from-math-to-motion/ 
+            // https://blog.maximeheckel.com/posts/cubic-bezier-from-math-to-motion/
             // there could be a function for inbetween keyframes and one for the entire transition
 
-
-            // this.setColors(newColor as rgbStripType["color"]).then(this.updateColors)
-
+            this.setColors(newColor as rgbStripType["color"], false);
         } else {
             console.warn("could not determine previous or next keyframe");
-
         }
-        setTimeout(() => this.effectUpdate(), CONFIG.ledRefreshRate);
+
+        setTimeout(() => {
+            this.effectUpdate();
+        }, CONFIG.ledRefreshRate);
     }
 
     public updateColors() {
         if (this.onOff === "off") {
-            this.gpioPorts?.red.pwmWrite(0)
-            this.gpioPorts?.green.pwmWrite(0)
-            this.gpioPorts?.blue.pwmWrite(0)
+            this.gpioPorts?.red.pwmWrite(0);
+            this.gpioPorts?.green.pwmWrite(0);
+            this.gpioPorts?.blue.pwmWrite(0);
         } else {
-            this.gpioPorts?.red.pwmWrite(this.color.red)
-            this.gpioPorts?.green.pwmWrite(this.color.green)
-            this.gpioPorts?.blue.pwmWrite(this.color.blue)
+            this.gpioPorts?.red.pwmWrite(this.color.red);
+            this.gpioPorts?.green.pwmWrite(this.color.green);
+            this.gpioPorts?.blue.pwmWrite(this.color.blue);
         }
     }
 
-    public stopEffect(): void { this.effectRunning = false; }
+    public stopEffect(): void {
+        this.effectRunning = false;
+    }
 }
