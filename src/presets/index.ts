272ac36b3b6e8d7f2fcb66ed500eca62a4122e3e
NilsGke <n.j.goeke@gmail.com>
2022-07-28 18:22:27 +0200

    reset presets if reloading

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -6,6 +6,7 @@ export const presets: preset[] = [];
 
 export const loadPresets = (): Promise<void> => {
     return new Promise((resolve, reject) => {
+        presets.length = 0;
         fs.readFile("./src/presets/presets.json", "utf8", (err, data) => {
             const dataPresets = JSON.parse(data) as preset[];
             dataPresets.forEach(preset => presets.push(preset))
