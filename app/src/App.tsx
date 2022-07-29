4eeb237a8d1eba6c0a59abc5c46278069a0cd83d
NilsGke <n.j.goeke@gmail.com>
2022-07-30 01:31:19 +0200

    new routes and removed old auto class adding thing and moved app div to page component itself

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,43 +1,41 @@
 import CircularProgress from "@mui/material/CircularProgress/CircularProgress";
-import { green } from "@mui/material/colors";
 import createTheme from "@mui/material/styles/createTheme";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import { useEffect, useRef } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ws from "./connection/connection";
 import Dashboard from "./Dashboard";
+import EffectsPage from "./Effects";
+import EffectsOverview from "./EffectsOverview";
 
 import "./styles/index.sass";
 
 const Router = () => {
-    const appRef = useRef<HTMLInputElement | null>(null);
-    useEffect(() => {
-        if (appRef.current !== null)
-            appRef.current.classList.add(
-                window.location.pathname.split("/")[1] || "dashboard"
-            );
-    }, []);
-
-
     if (ws.CONNECTING)
-        return <div className="dashboard loading"><CircularProgress /></div>;
+        return (
+            <div className="dashboard loading">
+                <CircularProgress />
+            </div>
+        );
 
     return (
-        <div id="app" ref={appRef}>
-            <ThemeProvider
-                theme={createTheme({
-                    palette: {
-                        mode: "dark",
-                    },
-                })}
-            >
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                    </Routes>
-                </BrowserRouter>
-            </ThemeProvider>
-        </div>
+        <ThemeProvider
+            theme={createTheme({
+                palette: {
+                    mode: "dark",
+                },
+            })}
+        >
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/effects" element={<EffectsOverview />} />
+                    <Route
+                        path="/effects/:effectName"
+                        element={<EffectsPage />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 };
 
