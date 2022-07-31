430eeeb5086bb07c649bcfa6191d2b5240f7733f
NilsGke <n.j.goeke@gmail.com>
2022-07-31 16:00:39 +0200

    added time difference message

--- app/src/connection/connection.ts
+++ app/src/connection/connection.ts
@@ -1,12 +1,33 @@
-import connection from "../connectionConfig.json"
+import connection from "../connectionConfig.json";
 
-console.log(connection.ip, connection.port)
+console.log(connection.ip, connection.port);
 
-const ws = new WebSocket(`ws://${connection.ip}:${connection.port}`, "echo-protocol")
+const ws = new WebSocket(
+    `ws://${connection.ip}:${connection.port}`,
+    "echo-protocol"
+);
 ws.onopen = (ev) => {
     console.log(ev);
+    getTimeDifference();
+};
+
+type timeDifferenceObj = { timeDifference: number };
+interface timeMessageEvent extends Event {
+    data: string;
 }
+const getTimeDifference = () => {
+    ws.addEventListener("message", timeMessageHandler);
+    ws.send(JSON.stringify({ time: Date.now() }));
+};
+const timeMessageHandler = (ev: Event) => {
+    const { timeDifference: serverTimeDifference } = JSON.parse(
+        (ev as timeMessageEvent).data
+    ) as timeDifferenceObj;
+    console.log(`\x1b[33mserver time difference: ${serverTimeDifference}ms`);
+    timeDifference = serverTimeDifference;
+    ws.removeEventListener("message", timeMessageHandler);
+};
 
-
+export let timeDifference: number = 0;
 
 export default ws;
