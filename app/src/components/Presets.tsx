d8de78b39765c910f261ef071a5b6521597348c0
NilsGke <n.j.goeke@gmail.com>
2022-08-12 00:02:45 +0200

    presets list now toggable

--- app/src/components/Presets.tsx
+++ app/src/components/Presets.tsx
@@ -1,34 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import addPreset from "../connection/addPreset";
 import { infoData } from "../connection/newData";
 import Preset from "./dashboard/Preset";
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 type props = {
-    data: infoData
-}
-export const Presets: React.FC<props> = ({
-    data
-}) => {
+    data: infoData;
+};
+export const Presets: React.FC<props> = ({ data }) => {
+    const [open, setOpen] = useState(false);
 
-    const [container] = useAutoAnimate<HTMLDivElement>(/* optional config */)
+    const [container] = useAutoAnimate<HTMLDivElement>(/* optional config */);
 
-    return <div id="presetsContainer" ref={container}>
-        {data.presets?.map(preset => <Preset active={preset.name === data.activePreset?.name} key={preset.id} data={preset} />)}
-        <div className="presetContainer">
-            <div
-                className="preset"
-                onClick={() => {
-                    const name = prompt("enter preset name: ", `newPreset#${data.presets.length + 1}`)
-                    if (name === null) return;
-                    let id = 0;
-                    while (data.presets.map(p => p.id).includes(id)) id++;
-                    addPreset({ name, id, strips: data.strips })
-                }}
-            >
-                <h2>+</h2>
+    return (
+        <div
+            id="presetsContainer"
+            ref={container}
+            className={open ? "open" : "closed"}
+        >
+            <div className="button">
+                <button onClick={() => setOpen(!open)}>
+                    Presets{" "}
+                    <div
+                        className={"arrow " + (open ? "open" : "closed")}
+                    ></div>
+                </button>
             </div>
+            {open ? (
+                <>
+                    {data.presets?.map((preset) => (
+                        <Preset
+                            active={preset.name === data.activePreset?.name}
+                            key={preset.id}
+                            data={preset}
+                        />
+                    ))}
+                    <div className="presetContainer">
+                        <div
+                            className="preset"
+                            onClick={() => {
+                                const name = prompt(
+                                    "enter preset name: ",
+                                    `newPreset#${data.presets.length + 1}`
+                                );
+                                if (name === null) return;
+                                let id = 0;
+                                while (
+                                    data.presets.map((p) => p.id).includes(id)
+                                )
+                                    id++;
+                                addPreset({ name, id, strips: data.strips });
+                            }}
+                        >
+                            <h2>+</h2>
+                        </div>
+                    </div>
+                </>
+            ) : null}
         </div>
-    </div>;
-}
+    );
+};
