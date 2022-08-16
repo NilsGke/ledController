8ab6e13985c88f955c63df225beeabc4655aa23a
NilsGke <n.j.goeke@gmail.com>
2022-08-16 12:22:33 +0200

    rewrote to be better

--- src/util/checkColor.ts
+++ src/util/checkColor.ts
@@ -1,13 +1,6 @@
 import { rgbStripType } from "../ledStrip/types";
 
-const checkColor = (color: rgbStripType["color"]): boolean => {
-    const colorArray = [color.red, color.green, color.blue];
-
-    // check that color is between 0 and 255
-    colorArray.forEach((c, i) => {
-        if (c > 255 || c < 0) return false;
-    });
-    return true;
-};
+const checkColor = (color: rgbStripType["color"]): boolean =>
+    [color.red, color.green, color.blue].every((val) => val <= 255 && val >= 0);
 
 export default checkColor;
