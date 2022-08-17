8a3a749b7c4523eaf4bb70ee6a28b0d2d38464d0
NilsGke <n.j.goeke@gmail.com>
2022-08-17 10:10:31 +0200

    added integrations folder

---
+++ integrations/discord/LedNotify.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name LedNotify
+ * @author NilsG#3330
+ * @description this plugin sends a notification to my led strips if i recieve a message
+ * @version 1.0.0
+ * @authorId 383501298584125441
+ * @source
+ * @updateurl
+ */
