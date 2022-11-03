24a700baf201f5a8fac95805857ec9fe4e72890f
NilsGke <n.j.goeke@gmail.com>
2022-11-03 22:03:11 +0100

    changed presetName to presetId on apply

--- app/src/connection/applyPreset.ts
+++ app/src/connection/applyPreset.ts
@@ -1,9 +1,9 @@
 import { preset } from "../../../src/presets/types";
 
-const applyPreset = (ws: WebSocket, presetName: preset["name"]): void => {
+const applyPreset = (ws: WebSocket, presetId: preset["id"]): void => {
     ws.send(
         JSON.stringify({
-            apply: presetName,
+            apply: presetId,
         })
     );
 };
